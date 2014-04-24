4c7605507d659f9694c62c27d923c4eeecefe8bc
arissitepu59 <arissitepu59@users.noreply.github.com>
2014-04-24 17:11:54 +0700

    one

---
+++ Fibonacci.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package fibonacci;
+import java.util.Scanner;
+/**
+ *
+ * @author Asisten
+ */
+public class Fibonacci {
+
+   public static void main(String[] args) {
+       Scanner s = new Scanner(System.in);
+       System.out.print("Input Angka : ");
+       int n = s.nextInt();
+       fibonacci(n);
+   }
+
+   public static void fibonacci(int n) {
+       if (n == 0) {
+           System.out.println("0");
+       } else if (n == 1) {
+           System.out.println("0 1");
+       } else {
+           System.out.print("0 1 ");
+           int a = 0;
+           int b = 1;
+           for (int i = 1; i < n; i++) {
+               int nextAngka = a + b;
+               System.out.print(nextAngka + " ");
+               a = b;
+               b = nextAngka;
+           }
+       }
+   }
+}
